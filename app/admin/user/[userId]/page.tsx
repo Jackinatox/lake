c9b5540db971ba2b4c3d1acdc4c7d787cfed958c
Larskanone <larsgrimm2602@gmail.com>
2025-01-30 23:27:27 +0100

    added display for user info detail

--- app/admin/user/[userId]/page.tsx
+++ app/admin/user/[userId]/page.tsx
@@ -1,3 +1,6 @@
+import { WidthFull } from '@mui/icons-material';
+import { Box, Sheet, Table } from '@mui/joy';
+import { TableBody, TableCell, TableHead, TableRow, TableSortLabel } from '@mui/material';
 import { Builder } from 'pterodactyl.js';
 import React from 'react'
 import { json } from 'stream/consumers';
@@ -19,8 +22,55 @@ async function User({ params }: { params: Promise<{ userId: string }> }) {
 
 
 
+
+
         return (
-            <div>{ user.fullName }</div>
+
+            <Box sx={{ overflow: "auto" }}>
+                <Box sx={{ width: "100%", display: "table", tableLayout: "fixed" }}>
+                    
+                        <Table borderAxis="x" variant="plain" width="500">
+                            <TableHead >
+                                <TableRow>
+                                    <TableCell>id          </TableCell>
+                                    <TableCell>externalId  </TableCell>
+                                    <TableCell>uuid        </TableCell>
+                                    <TableCell>internalId  </TableCell>
+                                    <TableCell>username    </TableCell>
+                                    <TableCell>email       </TableCell>
+                                    <TableCell>firstName   </TableCell>
+                                    <TableCell>lastName    </TableCell>
+                                    <TableCell>fullName    </TableCell>
+                                    <TableCell>language    </TableCell>
+                                    <TableCell>rootAdmin   </TableCell>
+                                    <TableCell>twoFactor   </TableCell>
+                                    <TableCell>updatedAt   </TableCell>
+                                    <TableCell>createdAt   </TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                <TableRow>
+                                    <TableCell>{user.id}          </TableCell>
+                                    <TableCell>{user.externalId}  </TableCell>
+                                    <TableCell>{user.uuid}        </TableCell>
+                                    <TableCell>{user.internalId}  </TableCell>
+                                    <TableCell>{user.username}    </TableCell>
+                                    <TableCell>{user.email}       </TableCell>
+                                    <TableCell>{user.firstName}   </TableCell>
+                                    <TableCell>{user.lastName}    </TableCell>
+                                    <TableCell>{user.fullName}    </TableCell>
+                                    <TableCell>{user.language}    </TableCell>
+                                    <TableCell>{user.rootAdmin}   </TableCell>
+                                    <TableCell>{user.twoFactor}   </TableCell>
+                                    <TableCell>{new Date(user.updatedAt).toLocaleString()}</TableCell>
+                                    <TableCell>{new Date(user.createdAt).toLocaleString()}</TableCell>
+                                </TableRow>
+                            </TableBody>
+                        </Table>
+                    
+                </Box>
+            </Box>
+
         )
     } catch (e) {
         return (
@@ -30,3 +80,6 @@ async function User({ params }: { params: Promise<{ userId: string }> }) {
 }
 
 export default User
+
+
+
