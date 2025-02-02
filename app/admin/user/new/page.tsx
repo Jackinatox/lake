90fdbf1eec389f19da816d7248f4412b8e3fcc86
Jackinatox <jakob.schulze1206@gmail.com>
2025-02-02 15:50:33 +0100

    Basic form validation feedback for user creation

--- app/admin/user/new/page.tsx
+++ app/admin/user/new/page.tsx
@@ -1,10 +1,12 @@
-"use client"
-
 import React from 'react'
 import newUserAction from './newUserAction'
-import { Button, FormControl, FormHelperText, FormLabel, Input } from '@mui/joy'
+import { Button, FormControl, FormLabel, Input } from '@mui/joy'
+import { FormMessage, Message } from '@/components/form-message';
 
-function createUser() {
+async function createUser(props: {
+  searchParams: Promise<Message>;
+}) {
+  const message = await props.searchParams;
   return (
     <>
       <form action={newUserAction}>
@@ -17,10 +19,15 @@ function createUser() {
           <Input name='email' placeholder="Email" />
         </FormControl>
         <FormControl>
+          <FormLabel>Nachname</FormLabel>
+          <Input name='lastname' placeholder="Nachname" />
+        </FormControl>
+        <FormControl>
           <FormLabel>Password</FormLabel>
           <Input name='password' placeholder="Passwort" />
         </FormControl>
 
+        <FormMessage message={message} />
 
         <Button type="submit" sx={{ marginTop: '1rem'}}>Submit</Button>
       </form>
