cfe7aa6894957a2dabb93177bb89f210c2f1da5a
Larskanone <larsgrimm2602@gmail.com>
2025-02-09 14:51:00 +0100

    added Table AdminPanel/Server

---
+++ app/admin/servers/ServerTable.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { ServerModel } from 'pterodactyl.js';
+import Table from '@mui/joy/Table';
+
+interface ServerTableProps {
+  servers: ServerModel[];
+}
+
+const ServerTable: React.FC<ServerTableProps> = ({ servers }) => {
+  return (
+    <Table aria-label="user table" borderAxis="both" variant="outlined" sx={{ tableLayout: "auto" }}>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Descr</th>
+          <th>User</th>
+          <th>Node</th>
+          <th>Allocation</th>
+          <th>Nest</th>
+          <th>Egg</th>
+          <th>Pack</th>
+          <th>externalId</th>
+          <th>internalId</th>
+          <th>uuid</th>
+          <th>identifier</th>
+          <th>user</th>
+        </tr>
+      </thead>
+      <tbody>
+        {servers.map((server) => (
+          <tr key={server.id}>
+            <td>{server.id}</td>
+            <td>{server.name}</td>
+            <td>{server.description}</td>
+            <td>{server.user}</td>
+            <td>{server.node}</td>
+            <td>{server.allocation}</td>
+            <td>{server.nest}</td>
+            <td>{server.egg}</td>
+            <td>{server.pack}</td>
+            
+            <td>{server.externalId}</td>
+            <td>{server.internalId}</td>
+            <td>{server.uuid}</td>
+            <td>{server.identifier}</td>
+            <td>{server.user}</td>
+            
+          </tr>
+        ))}
+      </tbody>
+    </Table >
+  );
+};
+
+export default ServerTable;
