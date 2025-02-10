9fb1591d57505999f57225499b55365e54ec78a6
Jackinatox <jakob.schulze1206@gmail.com>
2025-02-10 21:26:39 +0100

    pt user creation and api key save

--- lib/userApiKey.tsx
+++ lib/userApiKey.tsx
@@ -1,20 +1,18 @@
 async function createUserApiKey(userId: number): Promise<any> {
     const pturl = process.env.PTERODACTYL_URL;
+    const apiKey = process.env.PTERODACTYL_API_KEY;
     const url = `${pturl}/api/application/users/${userId}/api-keys`;
     const data = {
         description: 'API key for new user',
         allowed_ips: []
     };
 
-    console.log('Making request to:', url);
-    console.log('Request body:', JSON.stringify(data));
-
     try {
         const response = await fetch(url, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': 'Bearer YOUR_API_KEY' // Replace with your actual API key
+                'Authorization': `Bearer ${apiKey}` // Replace with your actual API key
             },
             body: JSON.stringify(data)
         });
@@ -23,12 +21,14 @@ async function createUserApiKey(userId: number): Promise<any> {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
 
-        console.log('Response Data:', response);
         const responseData = await response.json();
+        console.log('Response Data:', responseData);
         const { identifier } = responseData.attributes;
         const { secret_token } = responseData.meta;
 
-        return identifier + secret_token;
+        const key = identifier + secret_token;
+        console.log('Key: ', key);
+        return key;
     } catch (e) {
         console.error('PTUser API-Key Creation: ', e);
     }
