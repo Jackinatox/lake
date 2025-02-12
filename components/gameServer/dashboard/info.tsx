6c694697f42630a4f2f5ea9c3d274fef63bcb9c4
Larskanone <larsgrimm2602@gmail.com>
2025-02-12 23:07:54 +0100

    changed component structure

---
+++ components/gameServer/dashboard/info.tsx
@@ -0,0 +1,15 @@
+import { GameServerSettings } from "@/models/settings";
+import { Card } from "@mui/joy";
+
+interface InfoProps { settings: GameServerSettings }
+
+export function Info({ settings }: InfoProps) {
+
+    return (
+        <>
+            <Card size="sm">
+                {settings.egg} {settings.flavour} {settings.ver}
+            </Card>
+        </>
+    )
+}
