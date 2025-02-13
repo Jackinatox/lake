80c8a522d6c07811e5ec0e996eef9c0198cccc37
Jackinatox <jakob.schulze1206@gmail.com>
2025-02-14 00:59:11 +0100

    trying ext connection

--- components/gameServer/dashboard/console.tsx
+++ components/gameServer/dashboard/console.tsx
@@ -17,15 +17,17 @@ function Console({ server, ptApiKey }: serevrProps) {
 
     const startWebSocket = async () => {
       const wsCreds = await webSocket(server, ptApiKey);
-      console.log('api key und server: ', ptApiKey, server)
+      console.log('socket and token: ', wsCreds?.data.socket, wsCreds?.data.token);
 
-      //console.log('wsResponse: ', wsCreds)
-      // ws = new WebSocket(wsCreds?.socket);
+      ws = new WebSocket(wsCreds?.data.socket);
 
-      // ws.onopen = () => {
-      //   console.log("Connected to WebSocket");
-      //   ws.send(JSON.stringify({ token: wsCreds?.token })); // Authenticate      
-      // }
+      ws.onopen = () => {
+        console.log("Connected to WebSocket");
+        ws.send(JSON.stringify({
+          event: "auth",
+          args: [wsCreds?.data.token], // token as an array element
+        })); 
+      }
     }
 
     startWebSocket();
--- lib/Pterodactyl/webSocket.ts
+++ lib/Pterodactyl/webSocket.ts
@@ -1,26 +1,49 @@
 async function webSocket(serverId: string, apiKey: string) {
-  const pturl = process.env.NEXT_PUBLIC_PTERODACTYL_URL;
+  const response = await fetch('/api/ptero', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      apiKey: apiKey,  // or get it from somewhere secure
+      serverId: serverId,
+    }),
+  });
 
-  const url = `${pturl}/api/client/servers/${serverId}/websocket`;
-
-  console.log('ws cretion url: ', url)
-
-  try {
-    const response = await fetch(url, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${apiKey}`, // Replace with your actual API key
-      },
-    });
-    
-    console.log('ws creation function: ', JSON.stringify(response));
-
-    if (response.ok) {
-      return await response.json();
-    }
-  } finally {
+  if (!response.ok) {
+    // handle error
+    throw new Error(`Error: ${response.status}`);
   }
+
+    const data = await response.json();
+    console.log('Pterodactyl data:', data);
+
+    return data;
+
+
+
+  // const pturl = process.env.NEXT_PUBLIC_PTERODACTYL_URL;
+
+  // const url = `${pturl}/api/client/servers/${serverId}/websocket`;
+
+  // console.log('ws cretion url: ', url)
+
+  // try {
+  //   const response = await fetch(url, {
+  //     method: "GET",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       Authorization: `Bearer ${apiKey}`, // Replace with your actual API key
+  //     },
+  //   });
+    
+  //   console.log('ws creation function: ', JSON.stringify(response));
+
+  //   if (response.ok) {
+  //     return await response.json();
+  //   }
+  // } finally {
+  // }
 }
 
 export default webSocket;
