0ba57188d74b1feda9168237d7fb2876f8d3e01f
Jackinatox <jakob.schulze1206@gmail.com>
2025-02-15 17:24:08 +0100

    better ws implementation (start, stop, reAuth)

--- components/gameserver/dashboard/powerBtns.tsx
+++ components/gameserver/dashboard/powerBtns.tsx
@@ -5,7 +5,14 @@ import { useMediaQuery } from '@mui/material';
 import { CircleStop, Play, Power as Stop, RotateCcw } from 'lucide-react';
 import React from 'react'
 
-export function PowerBtns() {
+interface PowerBtnsProps {
+    loading: boolean;
+    onStop: () => void;
+    onStart: () => void;
+
+}
+
+export function PowerBtns({loading, onStop, onStart}: PowerBtnsProps) {
 
     const theme = useTheme();
     const isSmallScreen = useMediaQuery(theme.breakpoints.down('md'));
@@ -13,10 +20,10 @@ export function PowerBtns() {
     return (
         <>
             <ButtonGroup  variant='soft' spacing={{ xs: 0, sm: 0.4}} sx={{ display: "flex", justifyContent: isSmallScreen ? 'center' : 'flex-end' }}>
-                <Button sx={{ width: 110 }} size="sm" color="success" disabled={false} loading={false}> <Play size={16} /> &nbsp; Start </Button>
-                <Button sx={{ width: 110 }} size="sm" color="warning" disabled={false} loading={false}> <RotateCcw size={16} /> &nbsp; Restart </Button>
-                <Button sx={{ width: 110 }} size="sm" color="danger" disabled={false} loading={false}> <Stop size={16} /> &nbsp; Stop </Button>
-                <Button sx={{ width: 110 }} size="sm" color="danger" disabled={false} loading={false}> <CircleStop size={16} /> &nbsp; Kill </Button>
+                <Button onClick={onStart} sx={{ width: 110 }} size="sm" color="success" disabled={false} loading={loading}> <Play size={16} /> &nbsp; Start </Button>
+                <Button sx={{ width: 110 }} size="sm" color="warning" disabled={false} loading={loading}> <RotateCcw size={16} /> &nbsp; Restart </Button>
+                <Button onClick={onStop} sx={{ width: 110 }} size="sm" color="danger" disabled={false} loading={loading}> <Stop size={16} /> &nbsp; Stop </Button>
+                <Button sx={{ width: 110 }} size="sm" color="danger" disabled={false} loading={loading}> <CircleStop size={16} /> &nbsp; Kill </Button>
             </ButtonGroup >
         </>
     )
--- lib/Pterodactyl/webSocket.ts
+++ lib/Pterodactyl/webSocket.ts
@@ -19,31 +19,6 @@ async function webSocket(serverId: string, apiKey: string) {
     console.log('Pterodactyl data:', data);
 
     return data;
-
-
-
-  // const pturl = process.env.NEXT_PUBLIC_PTERODACTYL_URL;
-
-  // const url = `${pturl}/api/client/servers/${serverId}/websocket`;
-
-  // console.log('ws cretion url: ', url)
-
-  // try {
-  //   const response = await fetch(url, {
-  //     method: "GET",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //       Authorization: `Bearer ${apiKey}`, // Replace with your actual API key
-  //     },
-  //   });
-    
-  //   console.log('ws creation function: ', JSON.stringify(response));
-
-  //   if (response.ok) {
-  //     return await response.json();
-  //   }
-  // } finally {
-  // }
 }
 
 export default webSocket;
