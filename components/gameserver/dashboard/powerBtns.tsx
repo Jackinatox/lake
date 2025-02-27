63c569ccccf7edc2d918b4ca8f689d7f031c4b31
Larskanone <larsgrimm2602@gmail.com>
2025-02-27 23:17:37 +0100

    combined Stop/Kill, disable Btn depending state

--- components/gameserver/dashboard/powerBtns.tsx
+++ components/gameserver/dashboard/powerBtns.tsx
@@ -2,28 +2,32 @@
 
 import { Button, ButtonGroup, useTheme } from '@mui/joy'
 import { useMediaQuery } from '@mui/material';
-import { CircleStop, Play, Power as Stop, RotateCcw } from 'lucide-react';
 import React from 'react'
+import KillBtn from './killBtn';
+
 
 interface PowerBtnsProps {
     loading: boolean;
-    onStop: () => void;
     onStart: () => void;
-
+    onRestart: () => void;
+    onStop: () => void;
+    onKill: () => void;
+    state: string;
 }
 
-export function PowerBtns({loading, onStop, onStart}: PowerBtnsProps) {
+export function PowerBtns({ loading, onStart, onRestart, onStop, onKill, state }: PowerBtnsProps) {
 
     const theme = useTheme();
     const isSmallScreen = useMediaQuery(theme.breakpoints.down('md'));
 
     return (
         <>
-            <ButtonGroup  variant='soft' spacing={{ xs: 0, sm: 0.4}} sx={{ display: "flex", justifyContent: isSmallScreen ? 'center' : 'flex-end' }}>
-                <Button onClick={onStart} sx={{ width: 110 }} size="sm" color="success" disabled={false} loading={loading}> <Play size={16} /> &nbsp; Start </Button>
-                <Button sx={{ width: 110 }} size="sm" color="warning" disabled={false} loading={loading}> <RotateCcw size={16} /> &nbsp; Restart </Button>
-                <Button onClick={onStop} sx={{ width: 110 }} size="sm" color="danger" disabled={false} loading={loading}> <Stop size={16} /> &nbsp; Stop </Button>
-                <Button sx={{ width: 110 }} size="sm" color="danger" disabled={false} loading={loading}> <CircleStop size={16} /> &nbsp; Kill </Button>
+            <ButtonGroup variant='soft' spacing={{ xs: 0, sm: 0.5 }} sx={{ display: "flex", justifyContent: isSmallScreen ? 'center' : 'flex-end' }}>
+
+                <Button disabled={state !== 'offline'} loading={loading} sx={{ width: 90 }} size="sm" color="success" onClick={onStart}> Start </Button>
+                <Button disabled={state === 'offline'} loading={loading} sx={{ width: 90 }} size="sm" color="warning" onClick={onRestart}> Restart </Button>
+                <KillBtn loading={loading} onStop={onStop} onKill={onKill} state={state} />
+
             </ButtonGroup >
         </>
     )
