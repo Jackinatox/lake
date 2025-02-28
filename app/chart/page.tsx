64f28ce0978703d6f98eb43401f626a3641f9447
Jackinatox <58728356+Jackinatox@users.noreply.github.com>
2025-02-28 15:53:01 +0100

    Fisrt Implementation of a Chart

---
+++ app/chart/page.tsx
@@ -0,0 +1,10 @@
+import CPUChart from '@/components/gameserver/dashboard/graphs/CPUChart'
+import React from 'react'
+
+function page() {
+  return (
+    <CPUChart></CPUChart>
+  )
+}
+
+export default page
