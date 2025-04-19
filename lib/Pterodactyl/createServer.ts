cdff5098eb32b1dfec4c003c2ebc808fe3cbc509
Jackinatox <jakob.schulze1206@gmail.com>
2025-04-20 00:53:14 +0200

    Implement server creation, configuration and backup calculations; add global functions for disk size and backups

---
+++ lib/Pterodactyl/createServer.ts
@@ -0,0 +1,46 @@
+export function createServer(){
+    // TODO: Implement the createServer function
+}
+
+export interface NewServerOptions {
+    externalId?: string;
+    name: string;
+    user: number;
+    description?: string;
+    egg: number;
+    pack?: number;
+    image?: string;
+    startup: string;
+    limits: ServerLimits;
+    featureLimits: ServerFeatureLimits;
+    environment: {
+        [key: string]: any;
+    };
+    allocation?: {
+        default?: number;
+        additional: number[];
+    };
+    deploy?: {
+        locations?: number[];
+        dedicatedIp: boolean;
+        portRange: any[];
+    };
+    startWhenInstalled?: boolean;
+    skipScripts?: boolean;
+    outOfMemoryKiller?: boolean;
+}
+
+interface ServerLimits {
+    memory: number;
+    swap: number;
+    disk: number;
+    io: number;
+    cpu: number;
+}
+
+interface ServerFeatureLimits {
+    databases: number;
+    allocations: number;
+    backups: number;
+    split_limit: number;
+}
