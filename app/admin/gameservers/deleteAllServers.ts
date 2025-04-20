9a1f78b8a4cf4c473fb4367f5e1539d96fe06812
Jackinatox <jakob.schulze1206@gmail.com>
2025-04-21 01:09:06 +0200

    Fix authorization header typo and update server deletion logic; integrate deleteAllServers function in Gameservers component

--- app/admin/gameservers/deleteAllServers.ts
+++ app/admin/gameservers/deleteAllServers.ts
@@ -4,21 +4,21 @@ export async function deleteAllServers(){
     const response = await fetch(`${process.env.NEXT_PUBLIC_PTERODACTYL_URL}/api/application/servers`, {
         method: 'GET',
         headers: {
-            'Authorisation': `Bearer ${process.env.PTERODACTYL_API_KEY}`,
-            'Content-Type': 'application/json' 
+            'Authorization': `Bearer ${process.env.PTERODACTYL_API_KEY}`,
+            'Accept': 'application/json' 
         }
     });
-
     const data = await response.json();
     console.log('data: ', data);
 
     data.data.forEach(server => {
-        if (server.name === "serverino"){
+        if (server.attributes.name === "serverino"){
+            console.log('Deleting Server: ', JSON.stringify(server.attributes.id));
             fetch(`${process.env.NEXT_PUBLIC_PTERODACTYL_URL}/api/application/servers/${server.attributes.id}`, {
                 method: 'DELETE',
                 headers: {
-                    'Authorisation': `Bearer ${process.env.PTERODACTYL_API_KEY}`,
-                    'Content-Type': 'application/json' 
+                    'Authorization': `Bearer ${process.env.PTERODACTYL_API_KEY}`,
+                    'Accept': 'application/json' 
                 }
             });
         }        
