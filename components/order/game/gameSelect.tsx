314887f7ef852dd4578d88f761badf76033ea34a
Jackinatox <jakob.schulze1206@gmail.com>
2025-05-13 07:59:47 +0000

    ImageFecthing from Supabase

--- components/order/game/gameSelect.tsx
+++ components/order/game/gameSelect.tsx
@@ -1,35 +1,32 @@
 import React from 'react';
 import GameCard from './gameCard';
-import { Card } from '@/components/ui/card';
-import { Separator } from '@/components/ui/separator';
+import { createClient } from '@/utils/supabase/server';
 
-const game = [
-    {
-        id: '1',
-        name: 'minecraft',
-        fullName: 'Minecraft',
-        img: '/images/games/minecraft.jpg',
-        link: '',
-    },
-    {
-        id: 2,
-        name: 'satisfactory',
-        fullName: 'Satisfactory',
-        img: '/images/games/satisfactory.jpg',
-        link: '',
-    },
-];
 
-function GameSelect() {
+async function GameSelect() {
+    const supabase = await createClient();
+
+    const { data, error } = await supabase.from('GameData').select("*");
+    
+    const games = data.map(game => {
+        const imgName = `${game.name.toLowerCase()}.jpg`;
+        const {data: imgUrl} = supabase.storage.from('images').getPublicUrl(imgName);
+
+        return {
+            ...game,
+            imageUrl: imgUrl.publicUrl
+        }
+    })
+
     return (
         <>
             <div className='flex justify-center'>
                 <h2 className="text-3xl font-bold">Games</h2>
             </div>
             <div className="flex flex-wrap gap-4 justify-center">
-                {game.map((card) => {
+                {games.map((game) => {
                     return (
-                        <GameCard key={card.id} card={card} imgPath={card.img} />
+                        <GameCard key={game.id} card={game} imgPath={game.imageUrl} />
                     );
                 })}
             </div>
