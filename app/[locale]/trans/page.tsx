3db2006c742543aacac4de75a617710cbef3b86f
Jackinatox <jakob.schulze1206@gmail.com>
2025-05-19 21:13:46 +0200

    Finally Localization

--- app/[locale]/trans/page.tsx
+++ app/[locale]/trans/page.tsx
@@ -2,7 +2,7 @@ import { useTranslations } from 'next-intl'
 import React from 'react'
 
 function translation() {
-    const t = useTranslations();
+    const t = useTranslations('HomePage');
 
   return (
     <>{t('title')}</>
