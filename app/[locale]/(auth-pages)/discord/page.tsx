07ce7504d6869087b93425ec4e5df0399da9f0fd
Jackinatox <58728356+Jackinatox@users.noreply.github.com>
2025-05-26 18:55:33 +0200

    on the way to next auth

---
+++ app/[locale]/(auth-pages)/discord/page.tsx
@@ -0,0 +1,10 @@
+import SignInDiscord from '@/components/auth/authComponents'
+import React from 'react'
+
+function DiscordPage() {
+  return (
+    <div><SignInDiscord></SignInDiscord></div>
+  )
+}
+
+export default DiscordPage
--- components/Menu/language-switcher.tsx
+++ components/Menu/language-switcher.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React, { useTransition, useEffect, useState } from "react"
 import { Check, ChevronDown, Globe } from "lucide-react"
 
