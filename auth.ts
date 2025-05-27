f0fe8ccb6628bb49209caca56f2a5dca049ee234
Jakob Schulze <Jakob.Schulze1206@gmail.com>
2025-05-27 10:29:43 +0200

    DC Session

--- auth.ts
+++ auth.ts
@@ -1,6 +1,9 @@
+import { PrismaAdapter } from "@auth/prisma-adapter"
 import NextAuth from "next-auth"
 import Discord from "next-auth/providers/discord"
+import { prisma } from "./prisma"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [Discord],
 })
