a708c18de25a0ab3975a994ba9427fcc97b85ed5
Jackinatox <jakob.schulze1206@gmail.com>
2025-05-30 13:28:13 +0200

    prisma and removed unused types

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,32 +1,40 @@
 "use server"
 
 // import { getServerClient } from "@/lib/supabase"
-import type { Game, PerformanceGroup } from "@/models/config";
 import { createClient } from "@/utils/supabase/client";
+import { prisma } from "@/prisma";
 
-export async function fetchPerformanceGroups(): Promise<PerformanceGroup[]> {
-  const supabase = createClient();
-  const { data, error } = await supabase.from("Locations").select("*, CPU:CPU_Id (*), RAM:RAM_Id (*)").eq('Enabled', true).order('id');
+export async function fetchPerformanceGroups() {
+  const data = await prisma.location.findMany({ 
+    include: {
+      cpu: true,
+      ram: true,
+    },
+    where: {
+      enabled: true
+    }
 
-  if (error) {
-    console.error("Error fetching PFGroups:", error)
-    throw new Error("Failed to fetch PFGroups")
-  }
+  })
 
-  return data as PerformanceGroup[]
+  return data;
 }
 
-export async function fetchGames(gameId: number): Promise<Game> {
-  const supabase = createClient();
-  const { data, error } = await supabase.from("GameData").select("*").eq('id', gameId).single();
-  // console.log(data)
+export async function fetchGames(gameId: number){
+  const data = await prisma.gameData.findUnique({
+    where: { id: gameId },
+    select: {
+      id: true,
+      name: true,
+      data: true
+    }
+  });
 
-  // No cath here cause i need to knwo on the clkient if the game exists
-  if (error) {
-    console.error("Error fetching games:", error)
+  if (!data) {
+    console.error("Error fetching games: Game not found");
+    return null;
   }
-  
-  return data
+
+  return data;
 }
 
 export async function fetchGameFlavors(gameId: number) {
