920cc82f1f9b1ede2e79a198e320c916c55f94a6
Jackinatox <jakob.schulze1206@gmail.com>
2025-06-04 21:10:19 +0200

    somethign

---
+++ app/[locale]/booking2/[gameId]/bookServerPayment.ts
@@ -0,0 +1,19 @@
+"use server"
+
+import { prisma } from "@/prisma";
+import { ServerConfig } from "./page";
+
+export async function bookSerevrPayment(serverConfig: ServerConfig) {
+    serverConfig.hardwareConfig.pfGroupId;
+
+    const pfGroup = await prisma.location.findUnique({
+        where: { id: serverConfig.hardwareConfig.pfGroupId },
+        include: {
+            cpu: true,
+            ram: true,
+        }
+    });
+
+    console.log('pId: ', pfGroup)
+
+}
