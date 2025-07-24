efc00b0eabce8ccbe2101b7a03f80ca8716468d5
Jackinatox <jakob.schulze1206@gmail.com>
2025-07-24 19:50:55 +0000

    beautiful servers list

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,49 +43,44 @@ export default async function RootLayout({
 
   return (
     <html lang={locale} className={geistSans.className} suppressHydrationWarning>
-      <NextIntlClientProvider>
-        <body className="bg-background text-foreground">
+      <body className="bg-background text-foreground">
+        <NextIntlClientProvider>
           <ThemeProvider
             attribute="class"
             defaultTheme="system"
             enableSystem
             disableTransitionOnChange
           >
-            <main className="min-h-screen flex flex-col items-center">
-              <div className="flex-1 w-full flex flex-col items-center">
-                <SessionProvider>
-                  <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-                    <div className="w-full max-w-7xl flex justify-between items-center p-3 px-5 text-sm">
-                      <div className="flex items-center font-semibold">
-                        {/* left topbar */}
-
-                        <Link href="/" className="mr-4">Scyed</Link>
-                        <MainMenu locale={locale} />
-
-                        {/* left topbar end */}
-                      </div>
-
-                      <Profile />
+            <SessionProvider>
+              <div className="flex flex-col min-h-screen">
+                <nav className="w-full border-b border-b-foreground/10 h-16">
+                  <div className="w-full max-w-7xl mx-auto flex items-center justify-between p-3 px-5 text-sm">
+                    <div className="flex items-center font-semibold">
+                      {/* left topbar */}
+                      <Link href="/" className="mr-4">Scyed</Link>
+                      <MainMenu locale={locale} />
+                      {/* left topbar end */}
                     </div>
-                  </nav>
-                  <div className="flex flex-col gap-10 w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-5">
-
-                    {children}
-                    <Toaster />
-                    <SessionInfo />
-
+                    <Profile />
                   </div>
-                </SessionProvider>
+                </nav>
 
-                <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-                  <ThemeSwitcher />
+                <main className="flex-grow w-full max-w-screen-2xl mx-auto px-4 sm:px-6 lg:px-8 py-5">
+                  {children}
+                  {/* <SessionInfo /> */}
+                </main>
+
+                <footer className="w-full border-t">
+                  <div className="w-full max-w-7xl mx-auto flex items-center justify-center text-center text-xs gap-8 py-16">
+                    <ThemeSwitcher />
+                  </div>
                 </footer>
-
               </div>
-            </main>
+              <Toaster />
+            </SessionProvider>
           </ThemeProvider>
-        </body>
-      </NextIntlClientProvider>
+        </NextIntlClientProvider>
+      </body>
     </html>
   );
 }
