4656f1ab691596926db1362239f2cbf93ee8eaf6
Jackinatox <jakob.schulze1206@gmail.com>
2025-07-24 20:07:16 +0000

    old layout and suspense

--- app/[locale]/gameserver/page.tsx
+++ app/[locale]/gameserver/page.tsx
@@ -1,6 +1,6 @@
 "use server"
 import { createPtUserClient } from '@/lib/Pterodactyl/ptUserClient';
-import React from 'react'
+import React, { Suspense } from 'react'
 import ServersTable from './serversTable';
 import { auth } from '@/auth';
 import GameServersPage from './ServerTableNew';
@@ -9,7 +9,7 @@ const baseUrl = process.env.NEXT_PUBLIC_PTERODACTYL_URL
 const apiKey = process.env.PTERODACTYL_API_KEY;
 
 if (!baseUrl || !apiKey) {
-    throw new Error('PTERODACTYL_URL and PTERODACTYL_API_KEY must be defined');
+  throw new Error('PTERODACTYL_URL and PTERODACTYL_API_KEY must be defined');
 }
 
 
@@ -22,39 +22,10 @@ async function UserServer({ params }: { params: Promise<{ server_id: string }> }
     return <>Not logged in</>;
   }
 
-  const ptApiKey = session?.user.ptKey;
-
-  const data = await fetch(
-    `${baseUrl}/api/client`,
-    {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${ptApiKey}`,
-        // Authorization: `Bearer ptlc_dUhOyxSMfmFeeAbOyOzIZiuyOXs0qX0pLEOt5F76VpP`,
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-    },
-  )
-  
-  
-  const response = await data.json().then(data => data.data);
-
-
-  if (data.status === 403 || data.status === 404) {
-    console.error('auth error to pt API', data);
-    return <>Auth error</>
-  }
-
-  if (!data.ok) {
-    console.error('error from pt API', data);
-    return <>An error occured</>
-  }
-
-
-
   return (
-    <GameServersPage servers={response}></GameServersPage>
+    <Suspense fallback={<>loading</>}>
+      <GameServersPage></GameServersPage>
+    </Suspense>
   )
 }
 
