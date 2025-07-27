053a191caf4425b503b4a6e4496ebaffc6bcf34b
Jackinatox <jakob.schulze1206@gmail.com>
2025-07-27 18:18:22 +0000

    Booking duration in db and in purchase

--- app/[locale]/booking2/[gameId]/actions.ts
+++ app/[locale]/booking2/[gameId]/actions.ts
@@ -2,16 +2,19 @@
 
 import { ServerConfig } from "./page";
 import { auth } from "@/auth";
-import { headers } from "next/headers";
-import { stripe } from "@/lib/stripe";
+import { calculateTotal } from "@/components/booking2/hardware-config";
 import { prisma } from "@/prisma";
 
 export async function createServerOrder(serverConfig: ServerConfig): Promise<string> {
     serverConfig.hardwareConfig.pfGroupId
 
     const session = await auth();
+    const cpuCores = serverConfig.hardwareConfig.cpuCores;
+    const ramGB = serverConfig.hardwareConfig.ramGb;
+    const diskMb = serverConfig.hardwareConfig.diskMb;
+    const days = serverConfig.hardwareConfig.durationsDays;
 
-    if ((!session?.user) || serverConfig.hardwareConfig.cpuCores <= 0 || serverConfig.hardwareConfig.ramGb <= 0)
+    if ((!session?.user) || cpuCores <= 0 || ramGB <= 0)
         return '';
 
     const pfGroup = await prisma.location.findUnique({
@@ -22,18 +25,21 @@ export async function createServerOrder(serverConfig: ServerConfig): Promise<str
         }
     });
 
+    const expDate = new Date();
+    expDate.setDate(expDate.getDate() + days);
+
 
     const created = await prisma.serverOrder.create({
         data: {
             location: { connect: { id: pfGroup.id } },
             user: { connect: { email: session.user.email } },
-            cpuPercent: serverConfig.hardwareConfig.cpuCores * 100,
-            ramMB: serverConfig.hardwareConfig.ramGb * 1024,
-            diskMB: serverConfig.hardwareConfig.diskMb,
+            cpuPercent: cpuCores * 100,
+            ramMB: ramGB * 1024,
+            diskMB: diskMb,
             gameConfig: JSON.parse(JSON.stringify(serverConfig.gameConfig)),
             gameData: { connect: { id: serverConfig.gameConfig.gameId } },
-            price: (pfGroup.cpu.pricePerCore * serverConfig.hardwareConfig.cpuCores +
-                pfGroup.ram.pricePerGb * serverConfig.hardwareConfig.ramGb)
+            price: calculateTotal(pfGroup, cpuCores, ramGB, days).price,
+            expires: expDate
         }
     });
 
