5c78fd7ace1267b4b14bd427c688db3cf2b17f81
Jackinatox <jakob.schulze1206@gmail.com>
2025-08-18 19:47:30 +0000

    Chore: Server Upgarding

---
+++ app/[locale]/gameserver/[server_id]/upgrade/page.tsx
@@ -0,0 +1,26 @@
+import { auth } from '@/auth';
+import NotLoggedIn from '@/components/auth/NoAuthMessage';
+import UpgradeGameServerServer from '@/components/gameserver/Upgrade/UpgradeGameServerServer';
+import React from 'react'
+import Loading from './loading';
+
+
+async function UpgradePage({ params }: { params: { locale: string; server_id: string } }) {
+  const awaitedParams = await params;
+  const { server_id } = awaitedParams;
+  const session = await auth();
+
+  if (!session?.user) {
+    return <NotLoggedIn />
+  }
+
+  return (
+    <div className='flex justify-center'>
+      <React.Suspense fallback={<Loading />}>
+        <UpgradeGameServerServer serverId={server_id} apiKey={session.user.ptKey} />
+      </React.Suspense>
+    </div>
+  )
+}
+
+export default UpgradePage
