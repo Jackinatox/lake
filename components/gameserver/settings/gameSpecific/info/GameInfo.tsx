89306f737aec6a24773a64e757424d40669d4e87
Jackinatox <jakob.schulze1206@gmail.com>
2025-10-10 14:30:47 +0000

    refactor: add API key support to GameInfo and MinecraftInfo components, and update GameDashboard to pass API key

--- components/gameserver/settings/gameSpecific/info/GameInfo.tsx
+++ components/gameserver/settings/gameSpecific/info/GameInfo.tsx
@@ -7,15 +7,16 @@ import { FabricEggId, ForgeEggId, PaperEggId, VanillaEggId } from '@/app/GlobalC
 
 interface GameInfoProps {
     server: GameServer
+    apiKey: string
 }
 
-function GameInfo({ server }: GameInfoProps) {
+function GameInfo({ server, apiKey }: GameInfoProps) {
     return (
         <>
-            {server.egg_id === PaperEggId && <MinecraftInfo server={server} />}
-            {server.egg_id === VanillaEggId && <MinecraftInfo server={server} />}
-            {server.egg_id === ForgeEggId && <MinecraftInfo server={server} />}
-            {server.egg_id === FabricEggId && <MinecraftInfo server={server} />}
+            {server.egg_id === PaperEggId && <MinecraftInfo server={server} apiKey={apiKey} />}
+            {server.egg_id === VanillaEggId && <MinecraftInfo server={server} apiKey={apiKey} />}
+            {server.egg_id === ForgeEggId && <MinecraftInfo server={server} apiKey={apiKey} />}
+            {server.egg_id === FabricEggId && <MinecraftInfo server={server} apiKey={apiKey} />}
         </>
     )
 }
--- components/gameserver/settings/gameSpecific/info/MinecraftInfo.tsx
+++ components/gameserver/settings/gameSpecific/info/MinecraftInfo.tsx
@@ -1,14 +1,26 @@
+import { usePTEnv } from '@/hooks/usePTEnv'
 import { GameServer } from '@/models/gameServerModel'
 import { MinecraftConfig } from '@/models/gameSpecificConfig/MinecraftConfig'
 import React from 'react'
 
 interface MinecraftInfoProps {
-    server: GameServer
+    server: GameServer,
+    apiKey: string
 }
 
-function MinecraftInfo({ server }: MinecraftInfoProps) {
+function MinecraftInfo({ server, apiKey }: MinecraftInfoProps) {
+    const { value, error, loading } = usePTEnv("MINECRAFT_VERSION", server.identifier, apiKey);
+
+    if (error) {
+        return <div className="text-red-500">Error loading Minecraft version</div>
+    }
+
+    if (loading) {
+        return <div>Loading...</div>
+    }
+
     return (
-        <div>{(server.gameData.gameSpecificConfig as MinecraftConfig).flavor + " " + server.gameData.version}</div>
+        <div>{(server.gameData.gameSpecificConfig as MinecraftConfig).flavor + " " + value}</div>
     )
 }
 
