0842de1449a589386f33f4c455701e6ec99449ff
Jackinatox <jakob.schulze1206@gmail.com>
2025-12-01 11:08:55 +0100

    Prisma adapter fix

--- app/[locale]/admin/jobStatus/Job-Status-List.tsx
+++ app/[locale]/admin/jobStatus/Job-Status-List.tsx
@@ -1,9 +1,8 @@
-import { JobStatusCard } from './Job-Status-Card';
-import { ErrorSection } from './ErrorSection';
-import { JobStatus } from '../../../../worker/workerTypes';
 import prisma from '@/lib/prisma';
-
 import { env } from 'next-runtime-env';
+import { ErrorSection } from './ErrorSection';
+import { JobStatusCard } from './Job-Status-Card';
+
 
 // Async function to fetch job statuses
 async function getJobStatuses(): Promise<any[]> {
