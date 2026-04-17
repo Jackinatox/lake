c2b3c3d03eee909f5e55df6d6abfc52138d7180e
Jackinatox <jakob.schulze1206@gmail.com>
2026-04-18 00:53:13 +0200

    Enhance ConsoleV2: Improve log rendering and scrolling behavior

---
+++ scripts/updateMinecraftVersions.ts
@@ -0,0 +1,36 @@
+import prisma from '@/lib/prisma';
+
+try {
+    const newMinecraftVersions = await fetch(
+        'https://tea.scyed.de/Scyed/GameDataJsons/raw/branch/main/MinecraftVersions/MinecraftVersions.json',
+        {
+            headers: {
+                Authorization: `token ${process.env.GITTEA_MINECRAFT_JSONTOKEN}`,
+            }
+        }
+    );
+    if (!newMinecraftVersions.ok) {
+        throw new Error(
+            `Failed to fetch Minecraft versions: ${newMinecraftVersions.status} ${newMinecraftVersions.statusText}`,
+        );
+    }
+
+    console.log('fetched latest minecraft json');
+
+    const json = await newMinecraftVersions.json();
+
+    await prisma.gameData.update({
+        where: {
+            slug: 'minecraft',
+        },
+        data: {
+            data: json,
+        },
+    });
+
+    console.log('updated minecraft json in db');
+} catch (error) {
+    console.error('Error updating Minecraft versions:', error);
+}
+
+export {};
